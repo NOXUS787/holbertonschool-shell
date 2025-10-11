ec765529d5cdc9c8ce6eda21a50eaaee4df4cd23
NOXUS787 <alexander.rmn89@gmail.com>
2025-10-11 00:53:04 -0400

    Task 9

--- io_redirections_and_filters/EMACS.hs
+++ io_redirections_and_filters/EMACS.hs
@@ -1,12 +1,5 @@
 #!/bin/bash
-read -p "Enter the filename to create (e.g. myscript.sh): " fname
-if [ -e "$fname" ]; then
-  read -p "File exists. Overwrite? (y/N) " ans
-  case "$ans" in
-    [Yy]*) ;;
-    *) echo "Aborted."; exit 1 ;;
-  esac
-fi
-printf '%s\n' '#!/bin/bash' > "$fname"
-chmod +x "$fname"
-emacs "$fname"
+read -p "Enter the filename to create: " filename
+printf '#!/bin/bash\n' > "$filename"
+chmod +x "$filename"
+emacs "$filename"
